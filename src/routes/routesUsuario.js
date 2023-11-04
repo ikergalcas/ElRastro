3dc72052311941eb0c047988dae5f36d9396bb4d
yuste2002 <0610780201@uma.es>
2023-11-04 13:54:49 +0100

    get ubicacion

--- src/routes/routesUsuario.js
+++ src/routes/routesUsuario.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllUsuarios, createUsuario, editUsuario, deleteUsuario,getUsuarioNombre,getUsuarioValoracion} from '../controllers/UsuarioController.js'
+import { getAllUsuarios, createUsuario, editUsuario, deleteUsuario,getUsuarioNombre,getUsuarioValoracion, getUbiUsuario, calcularHuellaDeCarbono} from '../controllers/UsuarioController.js'
 
 const routerUsuario = express.Router()
 
@@ -9,5 +9,7 @@ routerUsuario.put('/:id', editUsuario)
 routerUsuario.delete('/:id', deleteUsuario)
 routerUsuario.post('/username',getUsuarioNombre)
 routerUsuario.post('/valoracion',getUsuarioValoracion)
-
+routerUsuario.get('/ubi/:idUsuario', getUbiUsuario)
+//routerUsuario.get('/ubi', getUbiUsuario)
+routerUsuario.get('/huella', calcularHuellaDeCarbono)
 export default routerUsuario
