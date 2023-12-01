9d83d49046bcfd7e93bef60d121408f97f848a4a
yuste2002 <0610780201@uma.es>
2023-12-01 18:35:07 +0100

    cositass login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Productos from "./productos/ShowProductos.js"
-
-
+import Login from "./login/Login.js"
+import DetallesProducto from "./productos/ShowDetallesProducto.js"
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Productos/>}/>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/productos" element={<Productos/>}/>
+          <Route path="/detallesProducto" element={<DetallesProducto/>}/>
         </Routes>
       </BrowserRouter>
   );
---
+++ frontend/src/productos/ShowDetallesProducto.js
@@ -0,0 +1,13 @@
+
+
+const CompShowDetallesProducto = () => {
+
+
+    return (
+        <div>
+            <p>Detalles producto</p>
+        </div>
+    )
+}
+
+export default CompShowDetallesProducto
