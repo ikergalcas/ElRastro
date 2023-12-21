96f6b37a1085a5efd30384d79ea53eac42d0b3ee
martaGonzalezP <mgonzalezpalmero@uma.es>
2023-12-21 21:08:55 +0100

    pago

--- frontend/src/pago/Pago.js
+++ frontend/src/pago/Pago.js
@@ -2,47 +2,23 @@ import React, { useState, useEffect, useRef} from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 
 const CompPago = () => {
+    
+    const paypal = useRef()
     const { idComprador, idProducto } = useParams()
 
     const [huellaCarbono, setHuellaCarbono] = useState()
     const [vendedor, setVendedor] = useState()
-    const [producto, setProducto] = useState()
+    const [producto, setProducto] = useState(null)
     const [checkout, setCheckOut] = useState(false)
     
-    const paypal = useRef()
-
     useEffect(() => {
-        var raw = JSON.stringify({
-            "idComprador" : idComprador,
-            "idProducto" : idProducto
-        })
-
-        fetch('http://localhost:3001/productos/huellaCarbonoNuevo', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: raw
-        }).then(response => response.json())
-        .then(data => {
-            
-            setProducto(data.producto)
-            setVendedor(data.vendedor)
-            setHuellaCarbono(data.huellaCarbono)
-            console.log(data.producto)
-            console.log(data.vendedor)
-            console.log(data.huellaCarbono)
-        })
-
-        // PayPal(producto.)
-    }, [])    
-
-    const PayPal = async (precio) => {
-        console.log(checkout)
-        if (!checkout) {
+        getDatos()
+    }, []) 
+    
+    useEffect(() => {
+        if (producto) {
             window.paypal.Buttons({
                 createOrder: (data, actions, err) => {
-                    setCheckOut(true)
                     return actions.order.create ({
                         intent: "CAPTURE",
                         purchase_units: [
@@ -50,7 +26,7 @@ const CompPago = () => {
                                 description: "Producto comprado",
                                 amount: {
                                     currency_code: "EUR",
-                                    value: precio
+                                    value: producto.maximaPuja
                                 }
                             }
                         ],
@@ -65,16 +41,44 @@ const CompPago = () => {
                 }
             }).render(paypal.current)
         }
+    }, [producto]);
+
+   
+    const getDatos = async () =>{
+        var raw = JSON.stringify({
+            "idComprador" : idComprador,
+            "idProducto" : idProducto
+        })
+
+        fetch('http://localhost:3001/productos/huellaCarbonoNuevo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: raw
+        }).then(response => response.json())
+        .then(data => {
+            setProducto(data.producto)
+            setVendedor(data.vendedor)
+           // setHuellaCarbono(data.huellaCarbono)
+            console.log(data.producto)
+            console.log(data.vendedor)
+           // console.log(data.huellaCarbono)
+        })
     }
 
     return(
+        producto && (
         <div>
             <h1>PAGAME HIJUEPUTA</h1> <br/>
-            <h3>Huella carbono {huellaCarbono && huellaCarbono}</h3> <br/>
+            <h3>Huella carbono {/*huellaCarbono && huellaCarbono*/}</h3> <br/>
             <h3>Vendedor {vendedor && vendedor.username}</h3> <br></br>
             <h3>Producto {producto && producto.titulo}</h3>
-            <div ref={paypal}></div>
+            <div style={{borderWidth: "6px", borderColor: "green"}}>
+                <div ref={paypal}></div>
+            </div>
         </div>
+        )
     )
 }
 
